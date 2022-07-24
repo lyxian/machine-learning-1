49ecf3ee50b7667d7c67d3db332df5a8ce236d4d
lyx <yxianlai@hotmail.com>
2022-07-24 17:02:26 +0800

    1-train: upd paths

--- img-num/1-train/localization.py
+++ img-num/1-train/localization.py
@@ -4,12 +4,11 @@ import numpy as np
 import cv2
 import os
 
-ZOOM = 1
 EXPAND = 4
-TRAIN_DIR = f'data/png/{ZOOM}'
-FILENAME = sample(os.listdir(TRAIN_DIR), len(os.listdir(TRAIN_DIR)))[0]
+TRAIN_DIR = f'data/png'
+FILENAME = '825.png' #sample(os.listdir(TRAIN_DIR), len(os.listdir(TRAIN_DIR)))[0]
 NUMBER = FILENAME.split('.')[0]
-SAVE_PATH = f'testing/{NUMBER}'
+SAVE_PATH = f'train/{NUMBER}'
 SAVE_IMAGE = f'{SAVE_PATH}/0.png'
 KERNEL_SIZE = (4, 4)
 SAVE_AS_TXT = True
@@ -21,8 +20,8 @@ if not os.path.exists(SAVE_PATH):
     os.mkdir(SAVE_PATH)
 else:
     print(f'Folder {SAVE_PATH} exists')
-    import sys
-    sys.exit()
+    # import sys
+    # sys.exit()
 
 # Process Image
 if 1:
@@ -41,15 +40,45 @@ if 1:
     regions, boundingBoxes = mser.detectRegions(tmp)
     saveStr = []
 
-    for idx, box in enumerate(boundingBoxes, start=1):
-        x, y, w, h = box
-        w += EXPAND; h += EXPAND
-        x -= EXPAND; y -= EXPAND
-        a = cv2.rectangle(tmp, (x, y), (x+w, y+h), (255, 0, 0), 1)
-        cv2.imwrite(f'{SAVE_PATH}/{idx}.png', img[y:y+h,x:x+w])
-        print(f'Saved to {SAVE_PATH}/{idx}.png ...')
-        saveStr += [f'{idx}: ({x}, {y}) -> ({x+w}, {y+h}) | w={w}, h={h}']
+    if 0:
+        d = {
+            'x1': [],
+            'y1': [],
+            'x2': [],
+            'y2': []
+        }
 
-    if SAVE_AS_TXT:
-        with open(f'{SAVE_PATH}/0.txt', 'w') as file:
-            file.write('\n'.join(saveStr))
+        for idx, box in enumerate(boundingBoxes, start=1):
+            x, y, w, h = box
+            w += EXPAND; h += EXPAND
+            x -= EXPAND; y -= EXPAND
+            if x < 0 or y < 0:
+                continue
+            x1 = x; y1 = y; x2= x+w; y2 = y+h
+            for i in d.keys():
+                d[i] += [locals()[i]]
+
+        def getThree(items, n=5):   
+            idx = [i + 1 for (x, y, i) in zip(items, items[1:], range(len(items))) if n < abs(x - y)]
+            res = [items[start:end] for start, end in zip([0] + idx, idx + [len(items)])]
+            # print(len(res), res)
+            return [_[0] for _ in res]
+
+        x1 = getThree(sorted(d['x1']))
+        y1 = sorted(d['y1'])[0]
+        x2 = sorted(getThree(sorted(d['x2'], reverse=True)))
+        y2 = sorted(d['y2'], reverse=True)[0]
+        print(x1,y1,x2,y2)
+    else:
+        for idx, box in enumerate(boundingBoxes, start=1):
+            x, y, w, h = box
+            w += EXPAND; h += EXPAND
+            x -= EXPAND; y -= EXPAND
+            a = cv2.rectangle(tmp, (x, y), (x+w, y+h), (255, 0, 0), 1)
+            cv2.imwrite(f'{SAVE_PATH}/{idx}.png', img[y:y+h,x:x+w])
+            print(f'Saved to {SAVE_PATH}/{idx}.png ...')
+            saveStr += [f'{idx}: ({x}, {y}) -> ({x+w}, {y+h}) | w={w}, h={h}']
+
+        if SAVE_AS_TXT:
+            with open(f'{SAVE_PATH}/0.txt', 'w') as file:
+                file.write('\n'.join(saveStr))
